e39e97a3ec723b8ebcb3280ec3c506b20cfa9f89
Lavinea Sens <lavineasens@hotmail.com>
2025-05-24 15:01:28 -0300

    Botão de adicionar produto na lista de desejos

--- assets/js/product.js
+++ assets/js/product.js
@@ -13,3 +13,14 @@ document.querySelector("#plus").addEventListener("click", () => {
     const currentQuantity = Number(input.value);
     input.value = currentQuantity + 1;
 });
+
+const addToWishlistButton = document.querySelector(".add-to-wishlist");
+const addToWishlistIcon = document.querySelector(".add-to-wishlist i");
+
+
+addToWishlistButton.addEventListener("click", () => {
+    if (addToWishlistIcon.className == 'fa fa-heart-o') 
+        addToWishlistIcon.className = 'fa fa-heart';
+    else
+        addToWishlistIcon.className = 'fa fa-heart-o'
+});

852c6cdc2793dfc9060596cc1001cab4bf34feea
Lavinea Sens <lavineasens@hotmail.com>
2025-05-25 21:07:08 -0300

    Adicionando comentários e removendo a opção "Catalog" do cabeçalho

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,14 +1,17 @@
 const input = document.querySelector("#quantity");
 
+// Botão de menos (-) para diminuir 1 ao valor do input de quantidade
 document.querySelector("#minus").addEventListener("click", () => {
     const currentQuantity = Number(input.value);
     
+    // Se a quantidade atual já for 1, mantém
     if (currentQuantity == 1)
         return;
 
     input.value = currentQuantity - 1;
 });
 
+// Botão de mais (+) para somar 1 ao valor do input de quantidade
 document.querySelector("#plus").addEventListener("click", () => {
     const currentQuantity = Number(input.value);
     input.value = currentQuantity + 1;
@@ -17,7 +20,8 @@ document.querySelector("#plus").addEventListener("click", () => {
 const addToWishlistButton = document.querySelector(".add-to-wishlist");
 const addToWishlistIcon = document.querySelector(".add-to-wishlist i");
 
-
+// Ao clicar em adicionar a lista de desejos, preenche o ícone de coração com cor-de-rosa.
+// Ao remover da lista de desejos, remove o preenchimento cor-de-rosa do ícone de coração.
 addToWishlistButton.addEventListener("click", () => {
     if (addToWishlistIcon.className == 'fa fa-heart-o') 
         addToWishlistIcon.className = 'fa fa-heart';
